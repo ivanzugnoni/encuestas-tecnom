eff75053a1bf4fa45405d06e376e2ffa7c8d0bad
Matias Caputti <matias.caputti@gmail.com>
2015-03-03 09:48:42 -0300

    Agrego controllers/views/services

--- scripts/directives/encuestaDirectives.js
+++ scripts/directives/encuestaDirectives.js
@@ -46,4 +46,83 @@ encuestasApp
 })
 
 
+.filter('JsDate', function () {
+    return function (fecha) {
+        return new Date(fecha);
+    }
+})
+
+.filter('JsonDateToJSDate', function () {
+    return function (fecha) {
+        return new Date(parseInt(fecha.replace('/Date(', '')));
+    }
+})
+
+.directive('inputBuscador', function () {
+    return {
+        restrict: "E",
+        scope: {
+            texto: '='
+        },
+        template:
+            "<div class=\"right-inner-addon\" style=\"margin-bottom:19px;\">"
+                   +"<span id=\"iconSpan\">"
+                   +"<i ng-show=\"texto.length > 0\" ng-click=\"texto = ''\" class=\"fa fa-times fa-2\"></i>"
+                   +"</span>"
+                   +"<input type=\"search\" class=\"form-control\" focus-me=\"true\" ng-model=\"texto\" placeholder=\"Filtro\">"
+               +"</div>",
+        transclude: true
+    };
+})
+
+.directive('masterHeader', function () {
+    return {
+        restrict: 'E',//<master-header></master-header> hace referencia a un elemento/etiqueta html
+        template:
+                "<div class=\"row\">"
+                    +"<div class=\"col-lg-12\">"
+                        +"<div class=\"page-header clearfix\">"
+                            +"<div class=\"\" ng-transclude>"
+                            +"</div>"
+                        +"</div>"
+                    +"</div>"
+                +"</div>",
+        transclude: true
+    };
+})
+
+.directive('headerTituloFull', function () {
+    return {
+        restrict: "E",
+        template:
+            "<div class=\"col-lg-12\">"
+                +"<div ng-transclude>"
+                +"</div>"
+            +"</div>",
+        transclude: true
+    };
+})
+
+.directive('headerTitulo', function () {
+    return {
+        restrict: "E",
+        template:
+            "<div class=\"col-lg-8\">"
+                +"<div ng-transclude>"
+                +"</div>"
+            +"</div>",
+        transclude: true
+    };
+})
+
+.directive('headerRight', function () {
+    return {
+        restrict: "E",
+        template:
+            "<div class=\"col-lg-4 text-right\" ng-transclude style=\"margin-top:28px;\">"
+            +"</div>",
+        transclude: true
+    };
+})
+
 ;
